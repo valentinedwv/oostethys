158b2b0b39c87cc53f168b627c6a4ba972ba8aff
jesper.zedlitz <jesper.zedlitz@dcf31090-a11b-0410-bfbf-9986d0f72e5d>
2009-06-11 13:40:31 +0000

    typo in class name remove unnecessary code

--- component/server/java/oostethys-java-server/src/test/java/org/oostethys/sos/test/ExceptionReporterTest.java
+++ component/server/java/oostethys-java-server/src/test/java/org/oostethys/sos/test/ExceptionReporterTest.java
@@ -4,7 +4,7 @@ import org.oostethys.sos.ExceptionReporter;
 
 import junit.framework.TestCase;
 
-public class ExceptionRerporterTest extends TestCase {
+public class ExceptionReporterTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -17,16 +17,8 @@ public class ExceptionRerporterTest extends TestCase {
 		String exceptionText = "operation supported are x y z";
 
 		String report = reporter.create(exceptionCode, locator, exceptionText);
-		System.out.println(report);
 
-		String s = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r"
-				+ "<ExceptionReport version=\"1.0\">"
-				+ "<Exception exceptionCode=\"OperationNotSupported\">"
-				+ "<ExceptionText>operation supported are x y z</ExceptionText>"
-				+ "</Exception></ExceptionReport>\r";
-		
 		assertTrue(report.contains("<ExceptionReport version=\"1.0\">"));
-		
 		assertTrue(report.contains("<ExceptionText>operation supported are x y z</ExceptionText>"));
 
 	}
