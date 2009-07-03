ff59a9083bd953d5ae26d1efdba06b101b4d00ab
jesper.zedlitz <jesper.zedlitz@dcf31090-a11b-0410-bfbf-9986d0f72e5d>
2009-07-03 10:11:09 +0000

    avoid possible null-pointer problems

--- component/server/java/oostethys-java-server/src/main/java/org/oostethys/model/VariablesConfig.java
+++ component/server/java/oostethys-java-server/src/main/java/org/oostethys/model/VariablesConfig.java
@@ -1,188 +1,178 @@
 package org.oostethys.model;
 
+import org.apache.commons.lang.StringUtils;
+
+import org.oostethys.model.impl.UnitsImpl;
+import org.oostethys.model.impl.VariableQuantityImpl;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.oostethys.model.impl.UnitsImpl;
-import org.oostethys.model.impl.VariableQuantityImpl;
 
 /**
  * <p>
  * This class maps a variable and a units label to corresponding URIs.
  * </p>
  * <hr>
- * 
+ *
  * @author : $Author: luisbermudez $
  * @version : $Revision: 1.4 $
  * @since : Feb 21, 2007
  */
-
 public class VariablesConfig {
+    /**
+     * @link composition
+     */
+    List<VariableQuantity> variablesConfig = new ArrayList<VariableQuantity>();
 
-	/**
-	 * @link composition
-	 */
-	java.util.List<VariableQuantity> variablesConfig = null;
+    public Iterator<VariableQuantity> getIterator() {
+        return variablesConfig.iterator();
+    }
 
-	public Iterator<VariableQuantity> getIterator() {
-		return variablesConfig.iterator();
-	}
+    public VariableQuantity getVariableByURI(final String uri) {
+        for (final VariableQuantity var : variablesConfig) {
+            if (StringUtils.equals(uri, var.getURI())) {
+                return var;
+            }
+        }
 
-	public VariableQuantity getVariableByURI(String uri) {
-		for (VariableQuantity var : variablesConfig) {
-		    if( StringUtils.equals(uri, var.getURI())) {
-			return var;
-		    }
-		}
-		return null;
+        return null;
+    }
 
-	}
+    /**
+     * Adds a variable and only sets the label
+     *
+     * @param standardName
+     */
+    public void addVariable(final String standardName) {
+        VariableQuantity var = new VariableQuantityImpl();
+        var.setLabel(standardName);
+        addVariable(var);
+    }
 
-	/**
-	 * Adds a variable and only sets the label
-	 * 
-	 * @param standardName
-	 */
-	public void addVariable(String standardName) {
+    public void addVariable(final String variableLabel,
+        final String variableURI, final String unitsLabel, final String unitsURI) {
+        this.addVariable(variableLabel, variableURI, unitsLabel, unitsURI, false);
+    }
 
-		VariableQuantity var = new VariableQuantityImpl();
-		var.setLabel(standardName);
-		addVariable(var);
+    public void addVariable(final String variableLabel,
+        final String variableURI, final String unitsLabel,
+        final String unitsURI, final boolean isCoordindate) {
+        VariableQuantity var = new VariableQuantityImpl(variableURI);
+        var.setLabel(variableLabel);
 
-	}
+        Units units = new UnitsImpl();
+        units.setLabel(unitsLabel);
 
-	public void addVariable(String variableLabel, String variableURI,
-			String unitsLabel, String unitsURI) {
+        var.setUnits(units);
+        var.setCoordinate(isCoordindate);
+        addVariable(var);
+    }
 
-		VariableQuantity var = new VariableQuantityImpl(variableURI);
-		var.setLabel(variableLabel);
-		Units units = new UnitsImpl();
-		units.setLabel(unitsLabel);
+    public void addVariable(final VariableQuantity var) {
+        if (!variablesConfig.contains(var)) {
+            variablesConfig.add(var);
+        }
+    }
 
-		var.setUnits(units);
-		addVariable(var);
+    public List<VariableQuantity> getVariables() {
+        return variablesConfig;
+    }
 
-	}
+    public void setVariables(final List<VariableQuantity> vars) {
+        variablesConfig = vars;
+    }
 
-	public void addVariable(String variableLabel, String variableURI,
-			String unitsLabel, String unitsURI, boolean isCoordindate) {
+    public String getLabel(final String uri) {
+        for (final VariableQuantity var : variablesConfig) {
+            if (StringUtils.equals(var.getURI(), uri)) {
+                return var.getLabel();
+            }
+        }
 
-		VariableQuantity var = new VariableQuantityImpl(variableURI);
-		var.setLabel(variableLabel);
-		Units units = new UnitsImpl();
-		units.setLabel(unitsLabel);
+        return null;
+    }
 
-		var.setUnits(units);
-		var.setCoordinate(isCoordindate);
-		addVariable(var);
+    public VariableQuantity getVariableGivenURI(final String uri) {
+        for (final VariableQuantity var : variablesConfig) {
+            if (StringUtils.equals(var.getURI(), uri)) {
+                return var;
+            }
+        }
 
-	}
+        return null;
+    }
 
-	public void addVariable(VariableQuantity var) {
-		if (variablesConfig == null) {
-			variablesConfig = new ArrayList<VariableQuantity>();
-		}
+    public List<VariableQuantity> getVariablesNotCoordinates() {
+        List<VariableQuantity> temp =
+            new ArrayList<VariableQuantity>(variablesConfig.size());
 
-		if (!variablesConfig.contains(var)) {
-			variablesConfig.add(var);
-		}
-	}
+        for (final VariableQuantity var : variablesConfig) {
+            if (!var.isCoordinate()) {
+                temp.add(var);
+            }
+        }
 
-	public List<VariableQuantity> getVariables() {
-		return variablesConfig;
-	}
+        return temp;
+    }
 
-	public void setVariables(List<VariableQuantity> vars) {
-		variablesConfig = vars;
-	}
+    //
+    public List<VariableQuantity> getCoordinates() {
+        List<VariableQuantity> temp =
+            new ArrayList<VariableQuantity>(variablesConfig.size());
 
-	public String getLabel(String uri) {
-		for (VariableQuantity var : variablesConfig) {
-			if (var.getURI().equals(uri)) {
-				return var.getLabel();
-			}
-		}
+        for (final VariableQuantity var : variablesConfig) {
+            if (var.isCoordinate()) {
+                temp.add(var);
+            }
+        }
 
-		return null;
-	}
+        return temp;
+    }
 
-	public VariableQuantity getVariableGivenURI(String uri) {
-		for (VariableQuantity var : variablesConfig) {
-			if (var.getURI().equals(uri)) {
-				return var;
-			}
-		}
+    public boolean isContainedInCoordinates(final String varName,
+        final String unitsName) {
+        List<VariableQuantity> vars = getCoordinates();
 
-		return null;
-	}
+        for (final VariableQuantity var : vars) {
+            if (StringUtils.equals(var.getLabel(), varName) &&
+                    (var.getUnits() != null) &&
+                    StringUtils.equals(var.getUnits().getLabel(), unitsName)) {
+                return true;
+            }
+        }
 
-	public List<VariableQuantity> getVariablesNotCoordinates() {
-		List<VariableQuantity> temp = new ArrayList<VariableQuantity>(
-				variablesConfig.size());
-		for (VariableQuantity var : variablesConfig) {
+        return false;
+    }
 
-			if (!var.isCoordinate()) {
-				temp.add(var);
-			}
+    public boolean isContainedInVariables(final String varName,
+        final String unitsName) {
+        VariableQuantity var = getVariable(varName, unitsName);
 
-		}
-		return temp;
-	}
+        return var != null;
+    }
 
-	//
+    public VariableQuantity getVariable(final String varName,
+        final String unitsName) {
+        for (final VariableQuantity var : variablesConfig) {
+            if (StringUtils.equals(var.getLabel(), varName) &&
+                    (var.getUnits() != null) &&
+                    StringUtils.equals(var.getUnits().getLabel(), unitsName)) {
+                return var;
+            }
+        }
 
-	public List<VariableQuantity> getCoordinates() {
-		List<VariableQuantity> temp = new ArrayList<VariableQuantity>(
-				variablesConfig.size());
-		for (VariableQuantity var : variablesConfig) {
+        return null;
+    }
 
-			if (var.isCoordinate()) {
-				temp.add( var);
-			}
-		}
-		return temp;
-	}
+    public VariableQuantity getVariable(final String varName) {
+        for (final VariableQuantity var : variablesConfig) {
+            if (StringUtils.equals(var.getLabel(), varName)) {
+                return var;
+            }
+        }
 
-	public boolean isContainedInCoordinates(String varName, String unitsName) {
-		List<VariableQuantity> vars = getCoordinates();
-		for (VariableQuantity var : vars) {
-			if (var.getLabel().equals(varName)
-					&& var.getUnits().getLabel().equals(unitsName)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean isContainedInVariables(String varName, String unitsName) {
-		VariableQuantity var = getVariable(varName, unitsName);
-		return var != null;
-	}
-
-	public VariableQuantity getVariable(String varName, String unitsName) {
-
-		for (VariableQuantity var : variablesConfig) {
-			if (var.getLabel().equals(varName)
-					&& var.getUnits().getLabel().equals(unitsName)) {
-				return var;
-			}
-		}
-		return null;
-	}
-
-	public VariableQuantity getVariable(String varName) {
-
-		for (VariableQuantity var : variablesConfig) {
-			if (var.getLabel().equals(varName)) {
-				return var;
-			}
-		}
-		return null;
-	}
-
-	
-	
-
+        return null;
+    }
 }
