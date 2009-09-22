cab7f8673992d957dd7ae6f9ebca57cc0a39899d
bermud <bermud@dcf31090-a11b-0410-bfbf-9986d0f72e5d>
2009-09-22 19:53:22 +0000

    Added on more example. It should get a random plot from any SOS ( SweCommon compliant)

--- trunk/component/client/java/sosReader/src/main/java/org/oostethys/sos/reader/example/ExampleGettingDataFor2DPlotting.java
+++ trunk/component/client/java/sosReader/src/main/java/org/oostethys/sos/reader/example/ExampleGettingDataFor2DPlotting.java
@@ -5,9 +5,10 @@ import java.net.URL;
 
 import org.oostethys.sos.reader.PlotData;
 import org.oostethys.sos.reader.SOSObservationSweCommonReader;
+import org.oostethys.sos.reader.SOSSimpleGeneralReader;
 
 public class ExampleGettingDataFor2DPlotting {
-	public static void main(String[] args) {
+	public static void main3(String[] args) {
 		// example MBARI SOS
 		try {
 			URL urlGetObservation = new URL(
@@ -23,28 +24,27 @@ public class ExampleGettingDataFor2DPlotting {
 			String unitsShort = plotData.getUnitsShort();
 			String unitsURL = plotData.getUnitsURI();
 			String variableURI = plotData.getVariableURI();
-			
-			System.out.println("title: "+title);
-			System.out.println("URI Variable: "+variableURI);
-			System.out.println("unitsShort: "+unitsShort);
+
+			System.out.println("title: " + title);
+			System.out.println("URI Variable: " + variableURI);
+			System.out.println("unitsShort: " + unitsShort);
 			System.out.println("FirstTimeStep: ");
-			System.out.println("   millisec: "+times[0]);
-			System.out.println("   values: "+values[0]);
-			
+			System.out.println("   millisec: " + times[0]);
+			System.out.println("   values: " + values[0]);
 
 		} catch (MalformedURLException e) {
-			
+
 			e.printStackTrace();
 		}
 
 	}
-	
+
 	public static void main2(String[] args) {
 		// example MBARI NG from IEEE 1451 using HTTP POST
 		try {
-			
+
 			String endPoint = "http://ww6.geoenterpriselab.com/SOSInterfaceToSTWS/STWS_SOS.asmx/GetCapabilities?request=GetCapabilities&service=SOS";
-			
+
 			URL urlGetObservation = new URL(
 					"http://mmisw.org/oostethys/sos?VERSION=1.0.0&SERVICE=SOS&REQUEST=GetObservation&offering=observationOffering_1455");
 			SOSObservationSweCommonReader reader = new SOSObservationSweCommonReader();
@@ -52,21 +52,52 @@ public class ExampleGettingDataFor2DPlotting {
 			String varURI = "http://mmisw.org/ont/cf/parameter/pressure";
 			reader.process(varURI);
 			PlotData plotData = reader.getPlotData();
-			String title = plotData.getTitle();
-			long[] times = plotData.getTimes();
-			double[] values = plotData.getValues();
-			String unitsShort = plotData.getUnitsShort();
-			String unitsURL = plotData.getUnitsURI();
-			String variableURI = plotData.getVariableURI();
+			getPlotData(plotData);
 			
-			System.out.println("title: "+title);
-			System.out.println("URI Variable: "+variableURI);
-			System.out.println("unitsShort: "+unitsShort);
-			System.out.println("FirstTimeStep: ");
-			System.out.println("   millisec: "+times[0]);
-			System.out.println("   values: "+values[0]);
-			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
+	
+	public static void getPlotData(PlotData plotData){
+		String title = plotData.getTitle();
+		long[] times = plotData.getTimes();
+		double[] values = plotData.getValues();
+		String unitsShort = plotData.getUnitsShort();
+		String unitsURL = plotData.getUnitsURI();
+		String variableURI = plotData.getVariableURI();
+
+		System.out.println("title: " + title);
+		System.out.println("URI Variable: " + variableURI);
+		System.out.println("unitsShort: " + unitsShort);
+		System.out.println("FirstTimeStep: ");
+		System.out.println("   millisec: " + times[0]);
+		System.out.println("   values: " + values[0]);
+		
+
+	}
+
+	// ///////
+
+	public static void main(String[] args) {
+		try {
+			SOSSimpleGeneralReader simpleReader = new SOSSimpleGeneralReader();
+			simpleReader
+					.processCapabilities("http://localhost:8080/oostethys/sos?VERSION=1.0.0&SERVICE=SOS&REQUEST=GetCapabilities");
+			
+			URL urlGetObservation = new URL(
+					simpleReader.getObservationURL());
+			SOSObservationSweCommonReader reader = new SOSObservationSweCommonReader();
+			reader.setURLGetObservation(urlGetObservation);
+			
+			String var = simpleReader.getVariableURL();
+
+			String varURI = var;
+			reader.process(varURI);
+			PlotData plotData = reader.getPlotData();
+			getPlotData(plotData);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
